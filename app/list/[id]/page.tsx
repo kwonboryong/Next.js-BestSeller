883b4d2ca22d62095549f286dd51a30ce06d576f
kwonboryong <kwonboryong@naver.com>
2024-11-15 23:31:39 +0900

    Create: 초기 경로 생성

---
+++ app/list/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>$카테고리 Books</h2>
+      <p>베스트셀러 각 카테고리 별 책의 리스트</p>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>The New York Times Best Seller Explorer</h2>
+      <p>베스트셀러 전체 리스트</p>
+    </div>
+  );
+}
