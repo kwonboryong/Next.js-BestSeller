b56c114371c5f81a1664b8e411fdd22b61e6e810
kwonboryong <kwonboryong@naver.com>
2024-11-16 13:24:03 +0900

    Update: 페이지 스타일 추가

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,5 @@
+import styles from "./About.module.css";
+
 export default function Home() {
   return <h2>about...</h2>;
 }
--- app/list/[id]/page.tsx
+++ app/list/[id]/page.tsx
@@ -1,6 +1,8 @@
+import styles from "../../../styles/Detail.module.css";
+
 export default function Home() {
   return (
-    <div>
+    <div >
       <h2>$카테고리 Books</h2>
       <p>베스트셀러 각 카테고리 별 책의 리스트</p>
     </div>
