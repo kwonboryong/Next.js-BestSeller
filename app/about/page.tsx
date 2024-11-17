7940cc51b6ce0c1b0e9cf2e9f9472ef934e9bf22
kwonboryong <kwonboryong@naver.com>
2024-11-17 23:26:05 +0900

    Update: about 페이지 스타일 수정

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,9 +2,17 @@ import styles from "./About.module.css";
 import { Metadata } from "next";
 
 export const metadata = {
-  title: "About"
-}
+  title: "About",
+};
 
 export default function Home() {
-  return <h2>about...</h2>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>About us</h2>
+      <p className={styles.description}>
+        Welcome th the official explorer for The New York Times Best Seller List
+        explorer.
+      </p>
+    </div>
+  );
 }
